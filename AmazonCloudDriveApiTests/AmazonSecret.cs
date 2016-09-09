7da6bdda46de8a139ec243c945114050bd7d3f02
Rambalac <rambalac+github@gmail.com>
2016-09-09 19:10:00 +0900

    Put Unit Test ClientId and ClientSecret into environment variables for AppVeyor

---
+++ AmazonCloudDriveApiTests/AmazonSecret.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Azi.Amazon.CloudDrive.Tests
+{
+    public static class AmazonSecret
+    {
+        // TODO Update to your values or create environment variables
+        public static string ClientId = Environment.GetEnvironmentVariable("ACDAPI_CLIENTID");
+        public static string ClientSecret = Environment.GetEnvironmentVariable("ACDAPI_CLIENTSECRET");
+    }
+}
