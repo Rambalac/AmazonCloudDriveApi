e43964ee47c1652037d8e4b73297d9cf9779d312
Rambalac <rambalac+github@gmail.com>
2016-10-24 16:53:21 +0900

    Fixed strong name

--- AmazonCloudDriveApi/Properties/AssemblyInfo.cs
+++ AmazonCloudDriveApi/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Azi.Amazon.CloudDrive")]
 [assembly: AssemblyCopyright("Rambalac")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("en-US")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.6.*")]
-[assembly: AssemblyFileVersion("1.2.6.0")]
+[assembly: AssemblyVersion("1.2.7.*")]
+[assembly: AssemblyFileVersion("1.2.7.0")]
--- AmazonCloudDriveApiTests/Properties/AssemblyInfo.cs
+++ AmazonCloudDriveApiTests/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Azi.Amazon.CloudDrive")]
 [assembly: AssemblyCopyright("Rambalac")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("en-US")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
